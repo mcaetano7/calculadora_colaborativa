dfd6bcb8bd047ada966bec5ad97cdc6ab8fd8bef
VictoriaMoraless <victoria.moraless@correo.ucu.edu.uy>
2025-03-19 19:15:00 -0300

    Implementación de la operacion feature-multiplication

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.WriteLine(Addition.Add(7, 2));
 Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
+//Esta clase implementa la operación multiplicacón
 public class Multiplication
 {
     public static int Multiply(int a, int b)
