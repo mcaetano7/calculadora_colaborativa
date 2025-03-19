4368c75237cf1b2a300d04f2a7aaba68cfbed8be
mcaetano7 <martina.caetano@correo.ucu.edu.uy>
2025-03-19 20:06:00 -0300

    Revert "suma"
    This reverts commit d77bb9467a9838b358aee898470edac59ff21f2c.

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -3,7 +3,6 @@ Console.WriteLine(Substraction.Substract(10, 1));
 Console.WriteLine(Multiplication.Multiply(3, 3));
 Console.WriteLine(Division.Divide(18, 2));
 
-//esta clase implementa la suma
 public class Addition
 {
     public static int Add(int a, int b)
